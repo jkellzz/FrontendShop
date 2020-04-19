1579e31fd8ff0c1ee1f4ebdc8f742d5b27def955
Hyugasi <isaiah.gordon@gmail.com>
2020-04-18 21:25:57 -0500

    Made the form for uploading documents

--- shop/src/components/upload/upload.js
+++ shop/src/components/upload/upload.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Upload() {
-  return <>This is upload</>;
+  return (
+    <div>
+      <h1>This is upload</h1>
+      <input type="text" placeholder="Upload your product" />
+      <input type="text" placeholder="Price" />
+      <input type="text" placeholder="Description" />
+      <input type="file" />
+      <button>Submit</button>
+    </div>
+  );
 }
 
 export default Upload;
