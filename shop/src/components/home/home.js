14ae34fa817794dd5817b793cda602880de9e76b
Johnathan Kelley <jkellz592@gmail.com>
2020-04-20 15:07:55 -0400

    Updated upload page

--- shop/src/components/home/home.js
+++ shop/src/components/home/home.js
@@ -5,7 +5,7 @@ import Search from '../search/search'
 
 function Home() {
   return ( <div>
-    <h1 className='title'>Your next purchase is a <img className='tiny' src='https://res.cloudinary.com/dtzbye6dy/image/upload/v1587394891/Screen_Shot_2020-04-20_at_11.00.30_AM_wnfrsi.png'/> away...</h1>;
+    <h1 className='title'>Your next purchase is a <img className='tiny' src='https://res.cloudinary.com/dtzbye6dy/image/upload/v1587394891/Screen_Shot_2020-04-20_at_11.00.30_AM_wnfrsi.png'/> away...</h1>
     <Search/>
     <Products/>
     </div>
