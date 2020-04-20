500943d915d59a93e3aee171caccf9f2107c7d7f
Hyugasi <isaiah.gordon@gmail.com>
2020-04-20 10:22:03 -0500

    made the cart component

--- shop/src/components/cart/cart.js
+++ shop/src/components/cart/cart.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Cart() {
-  return <></>;
+  return (
+    <div>
+      <h1>This is your Cart</h1>
+      <div>
+        <ul>
+          <li>item 1</li>
+          <li>item 2</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
