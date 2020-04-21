1824fc6c352b1870ef54b6ba98041f7afb612b09
Hyugasi <isaiah.gordon@gmail.com>
2020-04-21 15:28:28 -0500

    blah

--- shop/src/components/description/description.js
+++ shop/src/components/description/description.js
@@ -8,21 +8,23 @@ function Description(props) {
   console.log("props", props);
   console.log("itemDisplay", itemDisplay);
   return (
-    <div><h1 className='prodetails'>Product Details</h1>
-    <div className='horizontal'>
-      <img src={itemDisplay[0].picture} />
-      <div className='vert'>
-      <h3>Product Title</h3>
-      <p>{itemDisplay[0].title}</p>
-      <h3>Price</h3>
-      <p>{itemDisplay[0].price}</p>
-      <h3>Description</h3>
-      <p>{itemDisplay[0].description}</p>
-    </div>
-    </div>
-    <button className='submitbutton'>Add To Cart</button>
+    <div>
+      <h1 className="prodetails">Product Details</h1>
+      <div className="horizontal">
+        <img src={itemDisplay[0].picture} />
+        <div className="vert">
+          <h3>Product Title</h3>
+          <p>{itemDisplay[0].title}</p>
+          <h3>Price</h3>
+          <p>{itemDisplay[0].price}</p>
+          <h3>Description</h3>
+          <p>{itemDisplay[0].description}</p>
+        </div>
+      </div>
+      <button className="submitbutton">Add To Cart</button>
     </div>
   );
 }
 
 export default Description;
+ldskjaflkj;
