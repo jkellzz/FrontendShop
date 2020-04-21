e1e2775079b2dadb7f1853d5001dd21789391bfa
Hyugasi <isaiah.gordon@gmail.com>
2020-04-21 09:37:52 -0500

    working on things

--- shop/src/components/Products/Products.js
+++ shop/src/components/Products/Products.js
@@ -49,43 +49,7 @@ const Products = () => {
     );
   });
 
-  return (
-    // <MDBCol className='cardcontainer'>
-    //   <MDBCard className='card' style={{ width: "22rem" }}>
-    //     <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" waves />
-    //     <MDBCardBody>
-    //       <MDBCardTitle>Product Title</MDBCardTitle>
-    //       <MDBCardTitle>Price $$</MDBCardTitle>
-    //       <MDBBtn href="#">More Info</MDBBtn>
-    //     </MDBCardBody>
-    //   </MDBCard>
-    //   <MDBCard className='card' style={{ width: "22rem" }}>
-    //     <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" waves />
-    //     <MDBCardBody>
-    //       <MDBCardTitle>Product Title</MDBCardTitle>
-    //       <MDBCardTitle>Price $$</MDBCardTitle>
-    //       <MDBBtn href="#">More Info</MDBBtn>
-    //     </MDBCardBody>
-    //   </MDBCard>
-    //   <MDBCard className='card' style={{ width: "22rem" }}>
-    //     <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" waves />
-    //     <MDBCardBody>
-    //       <MDBCardTitle>Product Title</MDBCardTitle>
-    //       <MDBCardTitle>Price $$</MDBCardTitle>
-    //       <MDBBtn href="#">More Info</MDBBtn>
-    //     </MDBCardBody>
-    //   </MDBCard>
-    //   <MDBCard className='card' style={{ width: "22rem" }}>
-    //     <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" waves />
-    //     <MDBCardBody>
-    //       <MDBCardTitle>Product Title</MDBCardTitle>
-    //       <MDBCardTitle>Price $$</MDBCardTitle>
-    //       <MDBBtn href="#">More Info</MDBBtn>
-    //     </MDBCardBody>
-    //   </MDBCard>
-    // </MDBCol>
-    <div>{list}</div>
-  );
+  return <div>{list}</div>;
 };
 
 export default Products;
